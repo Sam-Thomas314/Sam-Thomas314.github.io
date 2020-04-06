3cb92559a4c4bab867e21f1804777573b4482884
Sam-Thomas314 <Samuelajt@gmail.com>
2020-04-06 06:28:32 -0930

    add Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from app import app
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 
@@ -7,6 +8,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 #db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
+
+
 
 #The lowercase "config" is the name of the Python module config.py,
 # and obviously the one with the uppercase "C" is the actual class.
